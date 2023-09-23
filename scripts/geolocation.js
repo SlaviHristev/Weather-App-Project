2be39b646fd1b0a588a33adc6cfd25fd5a6a7be8
SlaviHristev <Slavihristev97@gmail.com>
2023-09-23 11:38:20 +0300

    Improved my code by separating in functions and added geolocation script for future use.

---
+++ scripts/geolocation.js
@@ -0,0 +1,22 @@
+// const apiKey = 'f6a000fd8ec7467f8452ac982226ef7b';
+// const apiUrl = `https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}`;
+// import { getWeather } from "./script.js";
+
+// fetch(apiUrl)
+//     .then((response) => response.json())
+//     .then((data) => {
+//         if (data.city) {
+//                 const city = data.city;
+//                 console.log(city);
+//                 window.addEventListener('load', getWeather(city))
+//         } else {
+//             console.error('City information not available.');
+//         }
+//     })
+//     .catch((error) => {
+//         console.error('Error fetching city information:', error);
+//     });
+
+
+
+  
--- scripts/script.js
+++ scripts/script.js
@@ -1,78 +1,71 @@
 const inputField = document.getElementById('input');
-inputField.addEventListener('keypress', getWeather);
+inputField.addEventListener('keypress', handleKeyPress);
+
 import { createElement } from "./createEl.js";
 import { createElement2 } from "./createEl.js";
 import { clearWeatherInfo } from "./clearPage.js";
 
-async function getWeather(e) {
-    //Get the url needed and load the current and upcomming weather
-
+async function handleKeyPress(e) {
     if (e.key === "Enter") {
         e.preventDefault();
         clearWeatherInfo();
-
-        const currentWeatherUrl = `http://api.weatherapi.com/v1/forecast.json?key=26b117396f9a48aeb0a164550231809&q=${inputField.value}`;
-        const response = await fetch(currentWeatherUrl);
-        const data = await response.json();
-        console.log(data);
-
-        //destructure the data
-        let { current, forecast, location } = data;
-
-        //making city name and condition and attaching it to the DOM
-        const containerDiv = createElement('div', { class: 'container' }, null, document.body);
-        const currentWeatherDiv = createElement('div', { class: 'box', id: 'currentWeatherDiv' }, null, containerDiv);
-        const upcommingDiv = createElement('div', { class: 'box', id: 'upcommingWeatherDiv' }, null, containerDiv);
-        const cityNamePel = createElement2('p', { id: 'name', class: 'slideRight' }, location.name + '<br>' + current.condition.text, currentWeatherDiv);
-
-        //making temperature p and attaching it
-        const temperaturePel = createElement('p', { id: 'degrees', class: 'slideRight' }, current.temp_c, currentWeatherDiv);
-
-        // making the icon picture and attaching it
-        const imgElem = createElement('img', { id: 'icon', class:'fadeIn', src: current.condition.icon }, null, currentWeatherDiv);
-        imgElem.style.opacity = 1;
-
-        //making a table about the hourly degrees
-        const hourlyTable = createElement('table', { id: 'hourly' }, null, document.body);
-        hourlyTable.classList.add('slideRight')
-        const tbodyElement = createElement('tbody', { id: 'tbody' }, null, hourlyTable);
-        const hours = forecast.forecastday[0].hour;
-        hourlyTable.style.display = 'block';
-
-        for (let i = 0; i < hours.length - 2; i += 3) {
-
-            const newTheadEl = createElement('th', {}, hours[i].time, tbodyElement);
-            const newTdEl = createElement('td', {}, hours[i].temp_c, newTheadEl);
-            const newPicEl = createElement('img', { src: hours[i].condition.icon }, null, newTheadEl);
-        }
-
-        //getting the 7 day forecast
-        const sevenDayForecastUrl = `http://api.weatherapi.com/v1/forecast.json?key=26b117396f9a48aeb0a164550231809&q=${inputField.value}&days=7`
-        const res = await fetch(sevenDayForecastUrl);
-        const data2 = await res.json();
-
-        console.log(data2);
-        //TO DO: FIX destructuring here!
-        let { current2 = data2.current, forecast2 = data2.forecast, location2 = data2.location } = data2;
-
-        //filling the table with content
-        const upcommingTableEl = createElement('table', { id: 'upcomming'}, null, upcommingDiv);
-        const upcommingTbodyEl = createElement('tbody', { id: 'upcommingBody' }, null, upcommingTableEl);
-        upcommingTableEl.classList.add('slideDown');
-
-        for (let i = 0; i < forecast2.forecastday.length; i++) {
-            const trEl = createElement('tr', {}, null, upcommingTbodyEl);
-            const tdEl = createElement('td', {}, forecast2.forecastday[i].date, trEl);
-            const imgTd = createElement('td', {}, null, trEl);
-            const imgEl = createElement('img', { src: forecast2.forecastday[i].day.condition.icon }, null, imgTd);
-            const infoTdEl = createElement('td', {}, forecast2.forecastday[i].day.condition.text, trEl);
-            const tempInfoEl = createElement('td', {}, forecast2.forecastday[i].day.maxtemp_c + '/' + forecast2.forecastday[i].day.condition.text, trEl);
-        }
-        upcommingTableEl.style.display = 'table';
-
-        //creating conditions for the day table and filling it with data
-
-
+        const weatherData = await fetchWeatherData(inputField.value);
+        const weatherData2 = await fetchWeatherData2(inputField.value);
+        renderCurrentWeather(weatherData);
+        renderSevenDayForecast(weatherData2);
+        renderHourlyForecast(weatherData);
     }
 }
 
+async function fetchWeatherData(cityName) {
+    const currentWeatherUrl = `http://api.weatherapi.com/v1/forecast.json?key=26b117396f9a48aeb0a164550231809&q=${cityName}`;
+    const response = await fetch(currentWeatherUrl);
+    return await response.json();
+}
+async function fetchWeatherData2(cityName) {
+    const sevenDayForecastUrl = `http://api.weatherapi.com/v1/forecast.json?key=26b117396f9a48aeb0a164550231809&q=${cityName}&days=7`
+    const res = await fetch(sevenDayForecastUrl);
+    return await res.json();
+}
+
+function renderCurrentWeather(data) {
+    const containerDiv = createElement('div', { class: 'container' }, null, document.body);
+    const currentWeatherDiv = createElement('div', { class: 'box', id: 'currentWeatherDiv' }, null, containerDiv);
+    const cityNamePel = createElement2('p', { id: 'name', class: 'slideRight' }, data.location.name + '<br>' + data.current.condition.text, currentWeatherDiv);
+    const temperaturePel = createElement('p', { id: 'degrees', class: 'slideRight' }, data.current.temp_c, currentWeatherDiv);
+    const imgElem = createElement('img', { id: 'icon', class: 'fadeIn', src: data.current.condition.icon }, null, currentWeatherDiv);
+    imgElem.style.opacity = 1;
+}
+
+function renderHourlyForecast(data) {
+    const hourlyTable = createElement('table', { id: 'hourly' }, null, document.body);
+    hourlyTable.classList.add('slideRight');
+    const tbodyElement = createElement('tbody', { id: 'tbody' }, null, hourlyTable);
+    const hours = data.forecast.forecastday[0].hour;
+    hourlyTable.style.display = 'block';
+
+    for (let i = 0; i < hours.length - 2; i += 3) {
+        const newTheadEl = createElement('th', {}, hours[i].time, tbodyElement);
+        const newTdEl = createElement('td', {}, hours[i].temp_c, newTheadEl);
+        const newPicEl = createElement('img', { src: hours[i].condition.icon }, null, newTheadEl);
+    }
+}
+
+function renderSevenDayForecast(data) {
+    const containerDiv = document.querySelector('.container');
+    const upcommingDiv = createElement('div', { class: 'box', id: 'upcommingWeatherDiv' }, null, containerDiv);
+    const upcommingTableEl = createElement('table', { id: 'upcomming' }, null, upcommingDiv);
+    upcommingTableEl.classList.add('slideDown');
+    const upcommingTbodyEl = createElement('tbody', { id: 'upcommingBody' }, null, upcommingTableEl);
+    const forecast = data.forecast;
+    
+    for (let i = 0; i < forecast.forecastday.length; i++) {
+        const trEl = createElement('tr', {}, null, upcommingTbodyEl);
+        const tdEl = createElement('td', {}, forecast.forecastday[i].date, trEl);
+        const imgTd = createElement('td', {}, null, trEl);
+        const imgEl = createElement('img', { src: forecast.forecastday[i].day.condition.icon }, null, imgTd);
+        const infoTdEl = createElement('td', {}, forecast.forecastday[i].day.condition.text, trEl);
+        const tempInfoEl = createElement('td', {}, forecast.forecastday[i].day.maxtemp_c + '/' + forecast.forecastday[i].day.condition.text, trEl);
+    }
+    upcommingTableEl.style.display = 'table';
+}
